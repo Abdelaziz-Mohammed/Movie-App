fea9b09b13e2ee4e8c741b0b0708038cf46d3a06
Abdelaziz Mohamed <abdelazizmo2022@gmail.com>
2025-04-27 21:00:21 +0300

    Commit

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -7,11 +7,14 @@ function DetailsPage() {
   const params = useParams();
   const imageURL = useSelector(state => state.movie.imageURL);
   const {data} = useFetch(`/${params?.explore}/${params?.id}`);
-  const {data: castData } = useFetch(`/${params?.explore}/${params?.id}/credits`);
+  const {data: castData} = useFetch(`/${params?.explore}/${params?.id}/credits`);
+  const {data: similar} = useFetch(`/${params?.explore}/${params?.id}/similar`);
 
   // console.log(data);
   console.log(castData);
-  
+  console.log(castData.cast);
+  console.log(similar.results);
+
   return (
     <section className="">
       <div className='relative hidden md:block'>
@@ -26,7 +29,7 @@ function DetailsPage() {
       </div>
       {/* poster image */}
       <div className="container mx-auto px-4 pt-52 pb-16 md:pt-0 md:pb-16
-        flex flex-col md:flex-row items-center justify-start gap-6 md:gap-10
+        flex flex-col md:flex-row justify-start gap-6 md:gap-10
         ">
         <div className="-mt-28 relative w-fit mx-auto md:mx-0">
           <img src={imageURL+data?.poster_path} alt={data?.title} loading="eager"
@@ -76,10 +79,39 @@ function DetailsPage() {
               <span className="text-neutral-200">Revenue</span> : {data?.revenue}
             </p>
           </div>
-          {/*
-            Start from Here
-            Its time to 🥱😴😴
-          */}
+          {/* cast */}
+          <div>
+            <h2 className="text-xl text-white font-bold mb-5">
+              Cast :
+            </h2>
+            <div className="flex flex-wrap gap-y-8 gap-x-6">
+              {
+                castData?.cast?.map((cast, index) =>
+                  <div key={index}
+                    className="h-[120px] flex flex-col items-center justify-start w-20 gap-2"
+                  >
+                    {
+                      cast?.profile_path ?
+                      <img src={imageURL+cast?.profile_path} alt={cast.name || cast.original_name}
+                        className="w-20 h-20 rounded-full object-cover border border-neutral-700
+                        hover:scale-110 transition-all duration-100"
+                      /> :
+                      <div className="w-20 h-20 rounded-full bg-neutral-800 flex items-center justify-center">
+                        <span className="text-xs text-white/90">No Photo</span>
+                      </div>
+                    }
+                    <span className="text-neutral-400 text-xs text-center">
+                      {cast.name || cast.original_name}
+                    </span>
+                  </div>
+                )
+              }
+            </div>
+          </div>
+          {/* similar */}
+          <div>
+            similar movies/tv
+          </div>
         </div>
       </div>
     </section>
